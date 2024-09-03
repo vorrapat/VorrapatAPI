3b9be798d5f535c7720e0f0c2bcbbba1cbc0e80c
vorrapat <9beamdota@gmail.com>
2024-09-03 19:17:06 +0700

    อัปเดต app.use('uploads', express.static('uploads')))

--- VorrapatAPIserver.js
+++ VorrapatAPIserver.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/uploads', express.static('uploads'));
 
 // กำหนดการเก็บไฟล์ที่อัปโหลด
 const storage = multer.diskStorage({
